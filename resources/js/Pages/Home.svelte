637ed186819e5492326005b067f49ced6e3a463e
Afaandii <fandisantuy140@gmail.com>
2025-07-04 21:34:39 +0700

    hero section bg

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -7,15 +7,14 @@
 
 <!-- Hero section -->
 <section>
-    <div
-        class="hero min-h-screen"
-        style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);"
-    >
+    <div class="hero w-full h-[100vh]" style="hero">
         <div class="hero-overlay"></div>
         <div class="hero-content text-neutral-content text-center">
-            <div class="max-w-md">
-                <h1 class="mb-5 text-5xl font-bold">ALAMON GO!</h1>
-                <p class="mb-5">
+            <!-- <div class="max-w-md">
+                <h1 class="mb-5 text-5xl text-[#1f2122] font-bold">
+                    ALAMON GO!
+                </h1>
+                <p class="mb-5 text-[#1f2122]">
                     Minuman sehat kaya rasa dan manfaat, membantu meningkatkan
                     berat badan anda. Cukup seduh, nikmati langsung kesegaran
                     dari ALAMON GO!.
@@ -24,7 +23,18 @@
                     class="btn btn-outline btn-primary w-48 h-[50px] rounded-full text-white"
                     >Coba Sekarang!</button
                 >
-            </div>
+            </div> -->
         </div>
     </div>
 </section>
+
+<style>
+    .hero {
+        background-image: url("/public/storage/image/hero-bg.png");
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        height: 100vh;
+        width: 100%;
+    }
+</style>
