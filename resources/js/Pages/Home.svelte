645a6aaa6f43a9c84a82dcea0b4ebc18832eda17
Afaandii <fandisantuy140@gmail.com>
2025-07-04 16:36:09 +0700

    set navigasi bar

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -1,20 +1,52 @@
-<script></script>
+<script>
+</script>
 
-<nav class="bg-red-500 w-full h-auto">
-    <div class="flex justify-between align-items-center">
-        <div>
-            <img src="" alt="" />
+<nav class="bg-slate-100 w-full h-auto">
+    <div class="flex justify-around align-items-center">
+        <div class="avatar">
+            <div
+                class="ring-white w-16 mt-3 mb-3 rounded-full ring-2 ring-offset-2"
+            >
+                <img src="storage/image/user.jpg" alt="user.jpg" />
+            </div>
         </div>
         <div>
-            <ul class="flex justify-between align-items-center gap-5">
-                <li><a href="#">Beranda</a></li>
-                <li><a href="#">Tentang Kami</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Product</a></li>
+            <ul class="flex justify-between align-items-center gap-5 mt-5">
+                <li>
+                    <a
+                        href="#"
+                        class="text-2xl text-gray-950 hover:text-orange-500 font-mono"
+                        >Beranda</a
+                    >
+                </li>
+                <li>
+                    <a
+                        href="#"
+                        class="text-2xl text-gray-950 hover:text-orange-500 font-mono"
+                        >Tentang Kami</a
+                    >
+                </li>
+                <li>
+                    <a
+                        href="#"
+                        class="text-2xl text-gray-950 hover:text-orange-500 font-mono"
+                        >News</a
+                    >
+                </li>
+                <li>
+                    <a
+                        href="#"
+                        class="text-2xl text-gray-950 hover:text-orange-500 font-mono"
+                        >Product</a
+                    >
+                </li>
             </ul>
         </div>
         <div>
-            <button>Login</button>
+            <button
+                class="btn btn-outline btn-primary mt-5 w-32 rounded-full text-gray-950"
+                >Login</button
+            >
         </div>
     </div>
 </nav>
