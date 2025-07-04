21615d128779addb0cfe239d848034fb71da315d
Afaandii <fandisantuy140@gmail.com>
2025-07-04 20:08:46 +0700

    ganti logo web dan navbar

--- resources/js/Pages/Navigasi.svelte
+++ resources/js/Pages/Navigasi.svelte
@@ -1,39 +1,35 @@
 <nav class="bg-slate-100 w-full h-auto">
     <div class="flex justify-around align-items-center">
-        <div class="avatar">
-            <div
-                class="ring-white w-16 mt-3 mb-3 rounded-full ring-2 ring-offset-2"
-            >
-                <img src="storage/image/user.jpg" alt="user.jpg" />
-            </div>
+        <div class="w-16 my-auto">
+            <img src="storage/image/logos-nobg.png" alt="user.jpg" />
         </div>
         <div class="my-auto">
             <ul class="flex justify-center align-items-center gap-5">
                 <li>
                     <a
                         href="#"
-                        class="text-[20px] text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[16px] text-gray-950 hover:text-orange-500 font-poppins"
                         >Beranda</a
                     >
                 </li>
                 <li>
                     <a
                         href="#"
-                        class="text-[20px] text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[16px] text-gray-950 hover:text-orange-500 font-poppins"
                         >Tentang Kami</a
                     >
                 </li>
                 <li>
                     <a
                         href="#"
-                        class="text-[20px] text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[16px] text-gray-950 hover:text-orange-500 font-poppins"
                         >News</a
                     >
                 </li>
                 <li>
                     <a
                         href="#"
-                        class="text-[20px] text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[16px] text-gray-950 hover:text-orange-500 font-poppins"
                         >Product</a
                     >
                 </li>
@@ -41,7 +37,7 @@
         </div>
         <div class="my-auto">
             <button
-                class="btn btn-outline btn-primary w-32 rounded-full text-gray-950 hover:text-white"
+                class="btn btn-outline btn-primary w-32 h-[45px] rounded-full text-gray-950 hover:text-white"
                 >Login</button
             >
         </div>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
     <title>Alamon Go | Company Profile</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&family=Roboto&display=swap"
         rel="stylesheet">
+    <link rel="icon" type="image/svg+xml" href="storage/image/logos-bg.png" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
