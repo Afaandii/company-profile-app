3fd9131a1a7ca2080a4f69a5b8222feb95725f37
Afaandii <fandisantuy140@gmail.com>
2025-07-05 21:30:46 +0700

    pindah new file hero section dan edit navbar

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -1,43 +1,14 @@
 <script>
-    import Jumbroton from "./Jumbroton.svelte";
     import Navigasi from "./Navigasi.svelte";
+    import Hero from "./Hero.svelte";
+    import Jumbroton from "./Jumbroton.svelte";
 </script>
 
 <!-- Navigasi -->
 <Navigasi />
 
 <!-- Hero section -->
-<section class=" pt-[64px]">
-    <div class="hero w-full h-[100vh]" style="hero">
-        <div class="hero-overlay"></div>
-        <div class="hero-content text-neutral-content text-center">
-            <!-- <div class="max-w-md">
-                <h1 class="mb-5 text-5xl text-[#1f2122] font-bold">
-                    ALAMON GO!
-                </h1>
-                <p class="mb-5 text-[#1f2122]">
-                    Minuman sehat kaya rasa dan manfaat, membantu meningkatkan
-                    berat badan anda. Cukup seduh, nikmati langsung kesegaran
-                    dari ALAMON GO!.
-                </p>
-                <button
-                    class="btn btn-outline btn-primary w-48 h-[50px] rounded-full text-white"
-                    >Coba Sekarang!</button
-                >
-            </div> -->
-        </div>
-    </div>
-</section>
+<Hero />
 
+<!-- jumbotron -->
 <Jumbroton />
-
-<style>
-    .hero {
-        background-image: url("/public/storage/image/hero-bg.png");
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-        height: 100vh;
-        width: 100%;
-    }
-</style>
--- resources/js/Pages/Navigasi.svelte
+++ resources/js/Pages/Navigasi.svelte
@@ -1,35 +1,37 @@
 <nav class="fixed top-0 left-0 bg-white w-full h-auto z-50 shadow-md">
     <div class="flex justify-evenly align-items-center">
-        <div class="w-16 my-auto">
+        <div class="w-20 my-auto">
             <img src="storage/image/logos-nobg.png" alt="user.jpg" />
         </div>
         <div class="my-auto">
-            <ul class="flex justify-center align-items-center gap-5 my-auto">
+            <ul
+                class="flex justify-center align-items-center gap-[2rem] my-auto"
+            >
                 <li>
                     <a
                         href="#"
-                        class="text-[16px] no-underline text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[17px] no-underline font-bold text-gray-950 hover:text-orange-500 font-poppins"
                         >Beranda</a
                     >
                 </li>
                 <li>
                     <a
                         href="#"
-                        class="text-[16px] no-underline text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[17px] font-bold no-underline text-gray-950 hover:text-orange-500 font-poppins"
                         >Tentang Kami</a
                     >
                 </li>
                 <li>
                     <a
                         href="#"
-                        class="text-[16px] no-underline text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[17px] font-bold no-underline text-gray-950 hover:text-orange-500 font-poppins"
                         >News</a
                     >
                 </li>
                 <li>
                     <a
                         href="#"
-                        class="text-[16px] no-underline text-gray-950 hover:text-orange-500 font-poppins"
+                        class="text-[17px] font-bold no-underline text-gray-950 hover:text-orange-500 font-poppins"
                         >Product</a
                     >
                 </li>
@@ -38,9 +40,9 @@
         <div class="my-auto">
             <a
                 href="http://localhost:8000"
-                class="flex items-center gap-2 text-gray-950 no-underline text-[1rem] font-poppins hover:text-orange-500"
+                class="flex items-center gap-2 ml-3 text-gray-950 no-underline text-[1rem] font-poppins font-semibold hover:text-orange-500"
             >
-                <i class="fa-regular fa-circle-user text-[20px]"></i>
+                <i class="fa-regular fa-circle-user text-[23px]"></i>
                 <span>Login</span>
             </a>
         </div>
