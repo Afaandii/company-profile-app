2103fd25ec9aa8e052518a8486bddbc41d1cde80
Afaandii <fandisantuy140@gmail.com>
2025-07-06 23:02:50 +0700

    deleted comment

--- resources/js/Pages/Hero.svelte
+++ resources/js/Pages/Hero.svelte
@@ -1,21 +1,6 @@
 <section class=" pt-[64px]">
     <div class="hero w-full h-[100vh]" style="hero">
-        <div class="hero-content text-neutral-content text-center">
-            <!-- <div class="max-w-md">
-                <h1 class="mb-5 text-5xl text-[#1f2122] font-bold">
-                    ALAMON GO!
-                </h1>
-                <p class="mb-5 text-[#1f2122]">
-                    Minuman sehat kaya rasa dan manfaat, membantu meningkatkan
-                    berat badan anda. Cukup seduh, nikmati langsung kesegaran
-                    dari ALAMON GO!.
-                </p>
-                <button
-                    class="btn btn-outline btn-primary w-48 h-[50px] rounded-full text-white"
-                    >Coba Sekarang!</button
-                >
-            </div> -->
-        </div>
+        <div class="hero-content text-neutral-content text-center"></div>
     </div>
 </section>
 
