3c32fe870e9927b71e6cb72df773855fc923c8f1
Afaandii <fandisantuy140@gmail.com>
2025-07-08 23:40:26 +0700

    ganti icon footer

--- resources/js/Pages/Footer.svelte
+++ resources/js/Pages/Footer.svelte
@@ -2,9 +2,13 @@
     class="footer sm:footer-horizontal bg-neutral text-neutral-content p-10"
 >
     <aside>
-        <img src="storage/image/.png" alt="footer-logo" />
+        <img
+            src="storage/image/logo-footer.png"
+            alt="footer-logo"
+            class="w-24 -mt-4 -mb-4"
+        />
         <p>
-            ACME Industries Ltd.
+            PT ALAMON GO Industri Ltd.
             <br />
             Providing reliable tech since 2025
         </p>
@@ -12,19 +16,25 @@
     <nav>
         <h6 class="footer-title">Social</h6>
         <div class="grid grid-flow-col gap-4">
-            <a href="" class="text-3xl">
+            <a
+                href="https://github.com/Afaandii"
+                class="text-3xl text-white"
+                aria-label="github"
+            >
                 <i class="fa-brands fa-github"></i>
             </a>
-            <a href="" class="text-3xl">
+            <a href="" class="text-3xl text-white" aria-label="instagram">
                 <i class="fa-brands fa-instagram"></i>
             </a>
-            <a href="" class="text-3xl">
+            <a href="" class="text-3xl text-white" aria-label="linkdln">
                 <i class="fa-brands fa-linkedin"></i>
             </a>
         </div>
     </nav>
-    <p>
-        Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries
-        Ltd
-    </p>
+    <div class=" bg-slate-300">
+        <p class="text-black font-poppins">
+            Copyright © {new Date().getFullYear()} - All right reserved by ACME
+            Industries Ltd
+        </p>
+    </div>
 </footer>
