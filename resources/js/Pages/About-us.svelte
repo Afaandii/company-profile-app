2faba75e8460877dad068665e4065d2861a98376
Afaandii <fandisantuy140@gmail.com>
2025-07-09 17:34:03 +0700

    editing ui page about us

--- resources/js/Pages/About-us.svelte
+++ resources/js/Pages/About-us.svelte
@@ -1,11 +1,50 @@
 <script>
+    import Footer from "./Footer.svelte";
     import Navigasi from "./Navigasi.svelte";
 </script>
 
 <Navigasi />
 
-<section>
-    <div>
-        <h1 class="text-slate-900 mt-32">Halaman About us</h1>
+<section class="mt-32 mb-24 w-full lg:h-[100vh]">
+    <div
+        class="container bg-white rounded-3xl"
+        style="box-shadow: 0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 4px 6px -1px rgba(0, 0, 0, 0.1);"
+    >
+        <h1 class="font-poppins text-center font-semibold pt-3">
+            PT ALAMON GO Industri Ltd.
+        </h1>
+
+        <hr class="h-[2x] bg-black w-full p-0" />
+
+        <div class="row p-20">
+            <div class="col-12 col-lg-6 mb-4 mb-lg-0">
+                <img
+                    src="storage/image/logos-bg.png"
+                    alt="logos"
+                    class="w-full lg:w-[80%] rounded-2xl"
+                />
+            </div>
+            <div class="col-12 col-lg-6">
+                <p class="font-montserrat">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Cumque velit nam explicabo esse, fuga facilis, numquam
+                    inventore, accusantium rerum ad expedita nihil animi debitis
+                    iure error magni. Tempora, quo perspiciatis. Lorem ipsum
+                    dolor sit amet consectetur adipisicing elit. Perferendis
+                    officia corrupti deleniti magnam veritatis quas dolore, illo
+                    ut voluptatum reiciendis ipsa nulla ducimus placeat culpa
+                    molestias nobis assumenda voluptatem reprehenderit!
+                    Doloribus laboriosam aliquam deleniti ipsa sed labore
+                    blanditiis voluptatum provident nesciunt animi ut, dolorum
+                    enim aliquid maxime accusantium at veritatis exercitationem
+                    quae? Omnis accusamus optio molestiae incidunt eaque atque
+                    quos. Animi dicta iste placeat, tempora quibusdam molestias
+                    asperiores natus nam quidem odio dolores nobis, recusandae
+                    reprehenderit doloremque ipsa pariatur laboriosam.
+                </p>
+            </div>
+        </div>
     </div>
 </section>
+
+<Footer />
