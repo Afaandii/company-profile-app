6cbd9d627f1f7c44ab0c5638e95866c79e93a539
Afaandii <fandisantuy140@gmail.com>
2025-07-09 16:43:42 +0700

    inisialisasi halaman about

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/', function () {
     return Inertia::render('Home', ['name' => 'Afandi']);
 });
 
+Route::get('/about-us', function () {
+    return Inertia::render('About-us');
+});
+
 Route::get('/test', function () {
     return Inertia::render('Test');
 });
