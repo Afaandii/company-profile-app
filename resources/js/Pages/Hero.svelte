e3384e5ba48b4473f130df52e2175bb836d0f506
Afaandii <fandisantuy140@gmail.com>
2025-07-13 14:52:48 +0700

    tambah carousel hero section

--- resources/js/Pages/Hero.svelte
+++ resources/js/Pages/Hero.svelte
@@ -1,17 +1,64 @@
 <section>
-    <div class="hero w-full h-[100vh]" style="hero">
-        <div class="hero-overlay bg-black opacity-55"></div>
+    <!-- <div class="hero w-full h-[100vh]" style="hero">
         <div class="hero-content text-neutral-content text-center"></div>
+        <div class="hero-overlay bg-black opacity-55"></div>
+    </div> -->
+    <div
+        id="carouselExampleAutoplaying"
+        class="carousel slide"
+        data-bs-ride="carousel"
+    >
+        <div class="carousel-inner">
+            <div class="carousel-item hero active">
+                <img
+                    src="storage/image/hero-bg.png"
+                    class="d-block w-[100vw] h-[100vh] bg-cover bg-no-repeat bg-center"
+                    alt="poto png"
+                />
+            </div>
+            <div class="carousel-item">
+                <img
+                    src="storage/image/bg-hero2.png"
+                    class="d-block w-[100vw] h-[100vh] bg-cover bg-no-repeat bg-center"
+                    alt="..."
+                />
+            </div>
+            <div class="carousel-item">
+                <img
+                    src="storage/image/bg-hero3.png"
+                    class="d-block w-[100vw] h-[100vh] bg-cover bg-no-repeat bg-center"
+                    alt="..."
+                />
+            </div>
+        </div>
+        <button
+            class="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev"
+        >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+            class="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next"
+        >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </section>
 
 <style>
-    .hero {
+    /* .hero {
         background-image: url("storage/image/hero-bg.png");
         background-repeat: no-repeat;
         background-position: center;
         background-size: cover;
         height: 100vh;
         width: 100%;
-    }
+    } */
 </style>
