57c522bbe1d8fe18bcbf6386a3d1132e87bda508
Afaandii <fandisantuy140@gmail.com>
2025-08-03 21:34:11 +0700

    limit content data view news dashboard

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -41,7 +41,7 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__wobble" src="{{ asset('admin_lte/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo"
+            <img class="animation__wobble img-circle" src="{{ asset('storage/image/logos-bg.png') }}" alt="AdminLTELogo"
                 height="60" width="60">
         </div>
 
@@ -52,12 +52,6 @@
                 <li class="nav-item">
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
                             class="fas fa-bars"></i></a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index3.html" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Contact</a>
                 </li>
             </ul>
         </nav>
