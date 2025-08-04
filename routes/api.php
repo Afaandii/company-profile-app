5af4c336f24373d2f2095a50520b2454f252eb4c
Afaandii <fandisantuy140@gmail.com>
2025-08-04 18:51:21 +0700

    api dari backend untuk tampilan halaman news

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/news', [NewsController::class, 'homeNewsList']);
+Route::get('/berita', [NewsController::class, 'homeNewsList']);
+Route::get('/news', [NewsController::class, 'apiNewsList']);
