f6eb34b1fcfb74d2f967cef32aa21e29a8f1b235
Afaandii <fandisantuy140@gmail.com>
2025-08-26 20:09:33 +0700

    inisialisasi hal show product

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Alamon Go | Company Profile</title>
+    <title>Alamon Go</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Poppins&family=Roboto&display=swap"
         rel="stylesheet">
     <link rel="icon" type="image/svg+xml" href="{{ asset('storage/image/logos-bg.png') }}" />
--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Alamon Go | Dashboard</title>
+    <title>Alamon Go</title>
 
     <link rel="icon" type="image/svg+xml" href="{{ asset('/storage/image/logos-bg.png') }}" />
     <!-- Google Font: Source Sans Pro -->
