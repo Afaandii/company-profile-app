809ddb522c7c86b62e769ac140b995659eccd876
Afaandii <fandisantuy140@gmail.com>
2026-02-13 09:27:24 +0700

    fixx error vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/css/style.css",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
         svelte(),
